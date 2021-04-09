833805a9273e1e676141470d6a951b943d9aa4bb
Oluwafemi-iDEAR <ifesanmioluwafemi@gmail.com>
2021-04-09 17:33:13 +0100

    Updated

--- js/typewriter.js
+++ js/typewriter.js
@@ -54,4 +54,5 @@ var TxtType = function(el, toRotate, period) {
         css.type = "text/css";
         css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
         document.body.appendChild(css);
+    
     };
